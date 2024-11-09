3595a7272043936c0cedd28dad8bebb4109dfce0
Chris Lahey <clahey@clahey.net>
2024-11-09 12:03:16 -0500

    Add mongodb back to graphql.

--- src/app/api/graphql/resolvers.ts
+++ src/app/api/graphql/resolvers.ts
@@ -1,5 +1,62 @@
+import { Db, MongoClient, MongoServerError, ObjectId } from "mongodb";
+
+var clientPromise: Promise<MongoClient> | null = null;
+const testUser = new ObjectId(process.env.TEST_USER)
+
+function initializeClient(): Promise<MongoClient> {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const client = new MongoClient(process.env.MONGODB_URI!);
+            await client.connect();
+            resolve(client);
+        } catch (error) {
+            if (error instanceof MongoServerError) {
+                console.log(`Error worth logging: ${error}`); // special case for some reason
+            }
+            reject(error)
+        }
+    })
+}
+
+async function withDatabase<T>(block: ((db: Db) => Promise<T>)): Promise<T> {
+    if (clientPromise == null) {
+        clientPromise = initializeClient();
+    }
+    const client = await clientPromise;
+    const database = client.db("GolfScoreServer");
+    return block(database)
+}
+
 export const resolvers = {
+    Player: {
+        name: (parent: any) => parent.name
+    },
+    PlayerWithScores: {
+        player: async (playerWithScores: any) => withDatabase((db) =>
+            db.collection("Players").findOne({ _id: playerWithScores.id }))
+    },
+
+    Game: {
+        participants: (game: any) => game.players,
+        id: (game: any) => game._id,
+    },
+
     Query: {
-        hello: () => "Hello World"
+        players: async () => {
+            return withDatabase(async (db) => {
+                const collection = db.collection("Players");
+                const allData = await collection.find({ owner: testUser }).toArray()
+
+                return allData;
+            })
+        },
+        games: async () => {
+            return withDatabase(async (db) => {
+                const collection = db.collection("Games");
+                const allData = await collection.find({ owner: testUser }).toArray()
+
+                return allData;
+            })
+        },
     }
 }
--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -1,7 +1,22 @@
 import gql from "graphql-tag";
 
+
 export const typeDefs = gql`
+  type Player {
+    name: String!
+  }
+  type PlayerWithScores {
+    player: Player
+    scores: [Int]!
+  }
+  type Game {
+    id: String!
+    description: String!
+    participants: [PlayerWithScores!]!
+  }
+
   type Query {
-    hello: String
+    players: [Player!]!
+    games(id: String): [Game!]!
   }
 `
