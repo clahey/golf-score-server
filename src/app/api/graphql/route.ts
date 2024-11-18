add0ff49c1bb7c442878a8dfeed77b6d29145452
Chris Lahey <clahey@clahey.net>
2024-11-18 16:27:55 -0500

    Include stacktraces in errors in production.

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -7,6 +7,7 @@ import { resolvers } from "./resolvers";
 const server = new ApolloServer({
     typeDefs,
     resolvers,
+    includeStacktraceInErrorResponses: true,
 });
 
 const handler = startServerAndCreateNextHandler<NextRequest>(
